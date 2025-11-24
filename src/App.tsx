0bc3fa140646217ad903ada55d766fb3b6aae03d
marineags <agsmarine23@gmail.com>
2025-11-24 23:21:14 +0100

    update router

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Analyse from "./pages/analyse";
-import Header from "./components/Header"; // si tu crées un header
+import Header from "./components/Header.tsx"; // si tu crées un header
 
 function App() {
   return (
