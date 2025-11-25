b4092464b9d72eae405230870c36d9fac6ffe30e
DANIELLE EBWEA SONGUE <danielle.ewea237@gmail.com>
2025-11-25 12:13:19 +0100

    fonction fetch sur page analyse

--- src/pages/analyse.tsx
+++ src/pages/analyse.tsx
@@ -1,6 +1,28 @@
 // src/pages/Analyse.tsx
+import { useEffect, useState } from "react";
+
 function Analyse() {
-  return <h1>Page d'analyse</h1>;
+  const [tournages, setTournages] = useState<any[]>([]);
+
+  // 2. useEffect
+  useEffect(() => {
+    async function load() {
+      const response = await fetch(
+        "https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/lieux-de-tournage-a-paris/records?limit=20"
+      );
+      const data = await response.json();
+      console.log(data);
+      setTournages(data.results);
+    }
+    load();
+  }, []);
+  console.log(tournages);
+
+  return;
+  <div>
+    <h1>Page d'analyse</h1>
+    <p>On a récupéré {tournages.length} tournages pour les graphiques.</p>
+  </div>;
 }
 
 export default Analyse;
