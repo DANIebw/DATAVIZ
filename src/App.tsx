29892db00782b1aaf3fe1494ac1fca599ff221af
DANIELLE EBWEA SONGUE <danielle.ewea237@gmail.com>
2025-11-25 10:07:19 +0100

    function fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,28 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  return <h1>Dataviz</h1>;
+  // creation d'un State (memoire interne à mon composant)
+  const [tournages, setTournages] = useState([]);
+
+  // 2. useEffect
+  useEffect(() => {
+    async function load() {
+      const response = await fetch(
+        "https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/lieux-de-tournage-a-paris/records?limit=20"
+      );
+      const data = await response.json();
+      console.log(data);
+      setTournages(data.results);
+    }
+    load();
+  }, []);
+  console.log(tournages);
+
+  return (
+    <div>
+      <h1>Dataviz</h1>
+      <p>Nombre de tournages chargés : {tournages.length}</p>
+    </div>
+  );
 }
 export default App;
