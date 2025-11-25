49ff9c85d39cc77df58109756509576505f91bf6
DANIELLE EBWEA SONGUE <danielle.ewea237@gmail.com>
2025-11-25 10:13:03 +0100

    modification

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import Analyse from "./pages/analyse";
+import Header from "./components/Header.tsx"; // si tu crées un header
 
 function App() {
   return <h1>Dataviz</h1>;
 }
+
 export default App;
