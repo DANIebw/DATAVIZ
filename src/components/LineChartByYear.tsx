06c0f040a82633ac37e4b4d097158eb051da6f21
DANIELLE EBWEA SONGUE <danielle.ewea237@gmail.com>
2025-11-28 15:16:36 +0100

    udapte Linechart

--- src/components/LineChartByYear.tsx
+++ src/components/LineChartByYear.tsx
@@ -1,6 +1,6 @@
 // React : useMemo permet d'optimiser le calcul (ne se relance que si tournages change)
 import { useMemo } from "react";
-import { BarChart, Bar, XAxis, YAxis, Tooltip, CartesianGrid } from "recharts";
+import { LineChart, Bar, XAxis, YAxis, Tooltip, CartesianGrid } from "recharts";
 
 // Définition du type attendu en props
 // Chaque tournage peut ou non contenir l'année
@@ -9,13 +9,18 @@ type Props = {
     annee_tournage?: string;
   }[];
 };
+type YearCount = {
+  annee: string;
+  count: number;
+};
+
 
 // Component qui affiche un graphique des tournages par année
-export default function BarChartByYear({ tournages }: Props) {
+export default function LineChartByYear({ tournages }: Props) {
   // Transformation des données API → format adapté au graphique
   // Exemple sortie : [{ annee: "2020", count: 18 }, { annee: "2021", count: 25 }]
   const dataParAnnee = useMemo(() => {
-    return tournages.reduce((acc: any[], t) => {
+  const dataYear = tournages.reduce((acc: YearCount[], t) => {
       // Si l'année est absente, on met "Inconnue"
       const annee = t.annee_tournage || "Inconnue";
 
@@ -29,13 +34,15 @@ export default function BarChartByYear({ tournages }: Props) {
         // Si non → on ajoute une nouvelle entrée
         acc.push({ annee, count: 1 });
       }
-      return acc;
-    }, []);
-  }, [tournages]); // ⚡ Recalcule uniquement quand tournages change
+    
+    return acc;
+  }, [] as YearCount[]);
+ // ⚡ Recalcule uniquement quand tournages change
 
   // Affichage du graphique avec Recharts
   return (
-    <BarChart data={dataParAnnee} width={400} height={260}>
+
+    <LineChart data={dataParAnnee} width={400} height={260}>
       {/* Fond quadrillé du graphique */}
       <CartesianGrid strokeDasharray="3 3" />
 
@@ -50,6 +57,6 @@ export default function BarChartByYear({ tournages }: Props) {
 
       {/* Barres du graphique → colorées */}
       <Bar dataKey="count" fill="#6f1d1b" />
-    </BarChart>
+    </LineChart>
   );
-}
+  }
